c0e94da44098e4b34a6634c49007f62e06265255
Петрова Варвара <macblackriver@MacBook-Air-Mac.local>
2013-03-28 23:52:27 +0400

    Initial Commit

---
+++ Prototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Prototype
+//
+//  Created by Петрова Варвара on 28.03.13.
+//  Copyright (c) 2013 Петрова Варвара. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Prototype/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Prototype
+//
+//  Created by Петрова Варвара on 28.03.13.
+//  Copyright (c) 2013 Петрова Варвара. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
